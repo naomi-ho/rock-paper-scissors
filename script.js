47c7b27feda3be78b93cea2e85caeed3fe567074
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-01-23 11:43:03 -0500

    Implement scoring system

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const decisions = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+let roundResult;
+
 // Function that will randomly return either 'rock', 'paper' or 'scissors' for the computer's play
 function getComputerChoice() {
-    const decisions = ["rock", "paper", "scissors"];
     let computerChoicePosition = Math.floor(Math.random() * decisions.length);
     let computerChoice = decisions[computerChoicePosition];
     return computerChoice;
@@ -8,25 +12,31 @@ function getComputerChoice() {
 
 // Function that plays a single round of the game
 function playRound(playerSelection, computerSelection) {
-    typeof(playerSelection);
     convertedPlayerSelection = playerSelection.toLowerCase();
     if (convertedPlayerSelection === computerSelection) {
-        console.log("It's a tie");
+        playerScore++;
+        computerScore++;
+        roundResult = "It's a tie";
     } else if (convertedPlayerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You Win! Rock beats Scissors");
+        playerScore++;
+        roundResult = "You Win! Rock beats Scissors";
     } else if (convertedPlayerSelection === "rock" && computerSelection === "paper") {
-        console.log("You Lose, Paper beats Rock");
+        computerScore++;
+        roundResult = "You Lose, Paper beats Rock";
     } else if (convertedPlayerSelection === "paper" && computerSelection === "rock") {
-        console.log("You Win! Paper beats Rock");
-    } else if (convertedPlayerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You Lose! Scissors beats Paper");
+        computerScore++;
+        roundResult = "You Lose! Scissors beats Paper";
     } else if (convertedPlayerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You Win! Scissors beats Paper");
+        playerScore++;
+        roundResult = "You Win! Scissors beats Paper";
     } else if (convertedPlayerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You Lose! Rock beats Scissors");
+        computerScore++;
+        roundResult = "You Lose! Rock beats Scissors";
     } else {
-        console.log("Wrong input, please enter Rock, Paper, or Scissors");
+        roundResult = "Wrong input, please enter Rock, Paper, or Scissors";
     }
+    console.log(roundResult, playerScore, computerScore);
+    return (roundResult, playerScore, computerScore);
 }
 
 let playerSelection = prompt("Enter Rock, Paper, or Scissors", "");
