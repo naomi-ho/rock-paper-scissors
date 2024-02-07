3365f229cb26459e9f7e6802cd00a4b26d3a5454
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-07 18:28:57 +1100

    Create events where button clicks runs game with play selected hand

--- script.js
+++ script.js
@@ -6,38 +6,29 @@ let roundResult;
 const maxRoundNumber = 5;
 let gameResult;
 
-// function that will randomly return either 'rock', 'paper' or 'scissors' for the computer's play
-function getComputerChoice() {
-    let computerChoicePosition = Math.floor(Math.random() * decisions.length);
-    let computerChoice = decisions[computerChoicePosition];
-    return computerChoice;
-}
-
-// function that plays a best-of-five game that keeps score and reports a winner or loser at the end
 function game() {
     // function that plays a single round of the game
     function playRound(playerSelection, computerSelection) {
-        convertedPlayerSelection = playerSelection.toLowerCase();
-        if (convertedPlayerSelection === computerSelection) {
+        if (playerSelection === computerSelection) {
             playerScore++;
             computerScore++;
             roundResult = "It's a tie";
-        } else if (convertedPlayerSelection === "rock" && computerSelection === "scissors") {
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
             playerScore++;
             roundResult = "You win! Rock beats Scissors";
-        } else if (convertedPlayerSelection === "rock" && computerSelection === "paper") {
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
             computerScore++;
             roundResult = "You lose, Paper beats Rock";
-        } else if (convertedPlayerSelection === "paper" && computerSelection === "rock") {
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
             playerScore++;
             roundResult = "You win! Paper beats Rock";
-        } else if (convertedPlayerSelection === "paper" && computerSelection === "scissors") {
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
             computerScore++;
             roundResult = "You lose! Scissors beats Paper";
-        } else if (convertedPlayerSelection === "scissors" && computerSelection === "paper") {
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
             playerScore++;
             roundResult = "You win! Scissors beats Paper";
-        } else if (convertedPlayerSelection === "scissors" && computerSelection === "rock") {
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
             computerScore++;
             roundResult = "You lose! Rock beats Scissors";
         } else {
@@ -47,13 +38,38 @@ function game() {
         return (roundResult, playerScore, computerScore);
     }
 
-    // loop runs through 5 rounds
-    for (let round = 0; round < maxRoundNumber; round++) {
-        let playerSelection = prompt("Enter Rock, Paper, or Scissors", "");
-        let computerSelection = getComputerChoice();
-        console.log(playerSelection, computerSelection);
-        playRound(playerSelection, computerSelection);
+    // // loop runs through 5 rounds
+    // for (let round = 0; round < maxRoundNumber; round++) {
+    //     let playerSelection = prompt("Enter Rock, Paper, or Scissors", "");
+    //     let computerSelection = getComputerChoice();
+    //     console.log(playerSelection, computerSelection);
+    //     playRound(playerSelection, computerSelection);
+    // }
+
+    // function that will randomly return either 'rock', 'paper' or 'scissors' for the computer's play
+    function getComputerChoice() {
+    let computerChoicePosition = Math.floor(Math.random() * decisions.length);
+    let computerChoice = decisions[computerChoicePosition];
+    return computerChoice;
     }
+
+    // runs game with player playing rock
+    const playerSelectRock = document.querySelector('#rock');
+    playerSelectRock.addEventListener('click', () => {
+        playRound("rock", getComputerChoice());
+    });
+
+    // runs game with player playing scissors
+    const playerSelectScissors = document.querySelector('#scissors');
+    playerSelectScissors.addEventListener('click', () => {
+        playRound("scissors", getComputerChoice());
+    });
+
+    // runs game with player playing paper
+    const playerSelectPaper = document.querySelector('#paper');
+    playerSelectPaper.addEventListener('click', () => {
+        playRound("paper", getComputerChoice());
+    });
 
     // calculate and reports the game's winner
     if (playerScore === computerScore) {
